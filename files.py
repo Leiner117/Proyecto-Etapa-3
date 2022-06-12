2e68e22741180fe6208f68167a500c40e0ec3180
Leiner117 <lealvarado@estudiantec.cr>
2022-06-12 07:30:42 -0600

    update

--- files.py
+++ files.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 from tkinter import messagebox
 from obActivities import activities
 import functions_students
+from obEmotions import emotions
 def file_activities(student):
     try:
         fileActivities = open("Activities.txt","w")
@@ -22,9 +23,16 @@ def create_listemotions(lista):
     auxlist = []
     for i in lista:
         auxlist2 = []
-        auxlist2.append(i.hour)
+        auxlist2.append(datetime.strftime(i.hour,'%H:%M'))
         for a in i.list_emotions:
-             auxlist2.append(a)
+            
+             auxlist2.append(i.list_emotions[a])
+             '''auxlist2.append(a["sorrow_likelihood"])
+             auxlist2.append(a["anger_likelihood"])
+             auxlist2.append(a["surprise_likelihood"])
+             auxlist2.append(a["under_exposed_likelihood"])
+             auxlist2.append(a["blurred_likelihood"])
+             auxlist2.append(a["headwear_likelihood"])'''
         auxlist.append(auxlist2)
     return auxlist
         
@@ -43,15 +51,17 @@ def read_file():
                 line = line.split("-")
                 description = line[0]
                 course = line[1]
-                date = line[2]
+                date = datetime.strptime(line[2], '%Y/%m/%d')
                 start_time = line[3]
                 start_time = datetime.strptime(start_time, '%H:%M')
                 end_time = line[4]
                 end_time = datetime.strptime(end_time, '%H:%M')
                 status = line[5]
                 list_emotions = line[6].replace("\n",'')
-                ob = activities(description,course,date,start_time,end_time,status)
                 list_emotions = eval(list_emotions)
+                list_emotions = read_emotions(list_emotions)
+                ob = activities(description,course,date,start_time,end_time,status)
+                
                 ob.emotions = list_emotions
                 functions_students.list_students[0].activities.append(ob)
             else:
@@ -59,4 +69,20 @@ def read_file():
     except:
         
         messagebox.showerror("Lectura de archivos","Error en la lectura del archivo")
-        
+
+def read_emotions(list):
+    auxlist2 = []
+    
+    for i in list:
+        dic = {}
+        date = datetime.strptime(i[0], '%H:%M')
+        dic["joy_likelihood"] = i[1]
+        dic["sorrow_likelihood"] = i[2]
+        dic["anger_likelihood"] = i[3]
+        dic["surprise_likelihood"] = i[4]
+        dic["under_exposed_likelihood"] = i[5]
+        dic["blurred_likelihood"] = i[6]
+        dic["headwear_likelihood"] = i[7]
+        new = emotions(date,dic)
+        auxlist2.append(new)
+    return auxlist2
--- main_menu.py
+++ main_menu.py
@@ -22,7 +22,7 @@ def main_window():
     
     #boton agregar actividad
     tk.Button(main, text="Agregar actividades", command=lambda:add_activities.menuaddactivities(st), height=2,width=20).place(x=168, y=170)
-    
+    tk.Button(main, text="Concluir actividades", command=print, height=2,width=20).place(x=340, y=170)
     
     #boton Reconocimiento de emociones
     tk.Button(main, text="Reconocimiento de emociones", command=lambda:[emotion_recognition.capture_photos()], height=2,width=30).place(x=100, y=30)
@@ -30,9 +30,9 @@ def main_window():
     #concentracion
     tk.Button(main, text="Control de concentracion", command=print, height=2,width=30).place(x=100, y=100)
     tk.Button(main, text="Desactivar control", command=print, height=2,width=30).place(x=340, y=100)
-    tk.Button(main, text="Reportes", command=print, height=2,width=20).place(x=340, y=170)
+    tk.Button(main, text="Reportes", command=print, height=2,width=20).place(x=340, y=240)
     tk.Button(main, text="Graficos", command=print, height=2,width=20).place(x=168, y=240)
-    tk.Button(main, text="Salir ", command=lambda:[files.file_activities(st),close(main)], height=2,width=20).place(x=340, y=240)
+    tk.Button(main, text="Salir ", command=lambda:[files.file_activities(st),close(main)], height=2,width=20).place(x=260, y=300)
     
     #salir 
     
